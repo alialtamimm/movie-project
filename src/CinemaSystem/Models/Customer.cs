6b39412ca8c22e3cac82087ca32e7b0159d7a5ac
ali <alialtamimm@gmail.com>
2026-03-28 19:23:07 +0000

    changed a few references

--- src/CinemaSystem/Models/Customer.cs
+++ src/CinemaSystem/Models/Customer.cs
@@ -2,35 +2,38 @@ using System;
 
 namespace CinemaSystem.Models
 {
-    public class Member
+    public class Customer
     {
-        public int MemberId { get; set; }
+        public int CustomerId { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
+        public string Password { get; set; }
         public string MembershipType { get; set; } // standart, premium, vip
         public DateTime JoinDate { get; set; }
 
-        public Member()
+        public Customer()
         {
             FirstName = "";
             LastName = "";
             Email = "";
+            Password = "";
             MembershipType = "Standard";
             JoinDate = DateTime.Now;
         }
 
-        public Member(int memberId, string firstName, string lastName, string email, string membershipType)
+        public Customer(int id, string firstName, string lastName, string email, string password, string membershipType)
         {
-            MemberId = memberId;
+            CustomerId = id;
             FirstName = firstName;
             LastName = lastName;
             Email = email;
+            Password = password;
             MembershipType = membershipType;
             JoinDate = DateTime.Now;
         }
 
-        // returns discount based on membership type
+        // returns discount based on membership
         public decimal GetDiscount()
         {
             switch (MembershipType)
@@ -40,18 +43,17 @@ namespace CinemaSystem.Models
                 case "VIP":
                     return 0.20m; // 20% off
                 default:
-                    return 0.00m; // no discount for standart
+                    return 0.00m; // no discount
             }
         }
 
         public void DisplayInfo()
         {
-            Console.WriteLine($"ID: {MemberId}");
-            Console.WriteLine($"Name: {FirstName} {LastName}");
-            Console.WriteLine($"Email: {Email}");
-            Console.WriteLine($"Membership: {MembershipType}");
-            Console.WriteLine($"Joined: {JoinDate.ToShortDateString()}");
-            Console.WriteLine($"Discount: {GetDiscount() * 100}%");
+            Console.WriteLine($"  Name: {FirstName} {LastName}");
+            Console.WriteLine($"  Email: {Email}");
+            Console.WriteLine($"  Membership: {MembershipType}");
+            Console.WriteLine($"  Discount: {GetDiscount() * 100}%");
+            Console.WriteLine($"  Joined: {JoinDate.ToShortDateString()}");
         }
 
         public override string ToString()
